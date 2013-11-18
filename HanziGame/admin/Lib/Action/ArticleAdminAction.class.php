e3f8ce542ab8378b379d67c7e57f7b031fdeb87c
WangyfHYJY <wangyanfeng_2012@163.com>
2013-11-18 11:05:18 +0800

    article import

--- HanziGame/admin/Lib/Action/ArticleAdminAction.class.php
+++ HanziGame/admin/Lib/Action/ArticleAdminAction.class.php
@@ -13,9 +13,17 @@ class ArticleAdminAction extends BaseAction
 	}
 	function editArticle(){
 		if ($this->isPost()) {
-			$author=$_POST['author'];
-			$content=$_POST['content'];
-			echo $author.'-->'.$content;
+			$aid=$_POST['id'];
+			$data[author]=$_POST['author'];
+			$data[content]=$_POST['content'];
+			$DBarticle=M('article');
+			$res=$DBarticle->where("id='".$aid."'")->save($data);
+			if($res){
+				$this->success('更新成功','?m=ArticleAdmin&a=index');
+			}
+			else{
+				$this->error('编辑失败，请重试');
+			}
 		}else{
 			$aid=I('id');
 			$DBarticle=M('article');
@@ -24,11 +32,35 @@ class ArticleAdminAction extends BaseAction
 			$this->display();
 		}
 	}
+	function delArticle(){
+		$aid=I('id');
+		$DBarticle=M('article');
+		$res=$DBarticle->where("id='".$aid."'")->delete();
+		if($res){
+			$this->success('删除成功','?m=ArticleAdmin&a=index');
+		}else{
+			$this->error('删除失败，请重试！');
+		}
+	}
 	function articleimport(){
-		echo "导入";
+		if($this->isPost()){
+			$data['author']=$_POST['author'];
+			$data['content']=$_POST['comment'];
+			$data['in_time']=date('y-m-d H:i:s');
+			//data('y-m-d H:i:s',time())
+			$DBarticle=M('article');
+			$res=$DBarticle->add($data);
+			if ($res) {
+				$this->success('添加成功','?m=ArticleAdmin&a=index');
+			}else{
+				$this->error('添加失败');
+			}
+		}else{
+			$this->display();
+		}
 	}
 	function articleexport(){
-		echo "dd";
+		echo "文章导出";
 	}
 }
 ?>
